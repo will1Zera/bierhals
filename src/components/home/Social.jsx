f878f7f5dc30764134a387a5af97a2952e1bbd1c
William Bierhals <willambierhals>
2023-08-23 19:22:03 -0300

    :sparkles: Add home component

---
+++ src/components/home/Social.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Social = () => {
+  return (
+    <div className="home__social">
+        <a href="https://www.linkedin.com/in/williambierhals/" className="home__social-icon" target="_blank" rel="noreferrer"><i class="uil uil-linkedin-alt"></i></a>
+        <a href="https://github.com/will1Zera" className="home__social-icon" target="_blank" rel="noreferrer"><i className="uil uil-github-alt"></i></a>
+        <a href="https://www.instagram.com/william_bierhals/" className="home__social-icon" target="_blank" rel="noreferrer"><i className="uil uil-instagram"></i></a>
+    </div>
+  );
+}
+
+export default Social;
