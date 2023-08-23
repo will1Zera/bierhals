d70b1c72c17ac95697908884a0bdb2161f51953b
William Bierhals <willambierhals>
2023-08-23 15:56:57 -0300

    :sparkles: Add header component

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
+import React from 'react';
+import "./App.css"
+import Header from './components/header/Header.jsx'
 
-
-function App() {
-  
+const App = () => {
   return (
     <>
-      <h1>Hello</h1>
+      <Header />
     </>
   );
-
 }
 
 export default App;
