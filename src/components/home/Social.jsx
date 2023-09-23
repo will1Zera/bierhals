f6a144a8efa6f5d2689bd1b0d67ed10b6f568983
William Bierhals <willambierhals>
2023-09-23 18:10:00 -0300

    :recycle: Changing the title and instagram link

--- src/components/home/Social.jsx
+++ src/components/home/Social.jsx
@@ -8,7 +8,7 @@ const Social = () => {
     <motion.div className="home__social" initial={{opacity: 0, scale: 0}} animate={{opacity: 1, scale: 1}} transition={{duration: 0.8, ease: easeing}}>
         <a href="https://www.linkedin.com/in/williambierhals/" className="home__social-icon" target="_blank" rel="noreferrer"><i class="uil uil-linkedin-alt"></i></a>
         <a href="https://github.com/will1Zera" className="home__social-icon" target="_blank" rel="noreferrer"><i className="uil uil-github-alt"></i></a>
-        <a href="https://www.instagram.com/william_bierhals/" className="home__social-icon" target="_blank" rel="noreferrer"><i className="uil uil-instagram"></i></a>
+        <a href="https://www.instagram.com/will.bierhals/" className="home__social-icon" target="_blank" rel="noreferrer"><i className="uil uil-instagram"></i></a>
     </motion.div>
   );
 }
