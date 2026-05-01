8ca4a83aeae7cf8cd5eb8f03433ab041a21d8674
will1Zera <willambierhals@gmail.com>
2026-05-01 17:07:57 -0300

    Change profile and about images

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import "./about.css";
-import AboutImg from "../../assets/about.jpg";
+import AboutImg from "../../assets/about.jpeg";
 import CV from "../../assets/curriculo-william-bierhals.pdf";
 import Info from './Info';
 import { motion } from "framer-motion"
